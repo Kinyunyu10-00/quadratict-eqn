7a914e72aa3b429e21f8d0ce73ccf6ad2237e2b7
betson kinyunyu <kinyunyubetson@gmail.com>
2024-12-27 19:47:24 +0300

    second round

--- quadratic eq.cpp.cpp
+++ quadratic eq.cpp.cpp
@@ -5,4 +5,7 @@ using namespace std;
 
 void solveQuadratic(double a, double b, double c) {
     double discriminant = b*b - 4*a*c;
- 
+  double root1, root2;
+
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2*a);
